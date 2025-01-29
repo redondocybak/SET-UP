7463d67decbc7b31dd93a75964b6e9a50662383b
Alejandro Redondo Cybak <aredondocybak@gmail.com>
2025-01-29 09:17:01 -0500

    I am uploading what I did on Wedbesday

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-
-
+import { Card, Descriptions, Divider, List, Button} from 'antd';
+import dishes from '../src/assets/data/dishes.json'
+import DescriptionsItem from 'antd/es/descriptions/Item';
 
 function App() {
   return (
-    <div>
-      <span>MOno</span>
-      
-    </div>
+    
+    <Card title={'Order Title'} style={{ margin: 20 }}> 
+        <Descriptions bordered column={{lg: 1, md: 1, sm: 1}}>
+          <Descriptions.Item label='Customer'>Jose Ricardo Cuadra</Descriptions.Item>
+          <Descriptions.Item label='Customer Address'>Calle del Simio 4, Santo Domingo, Portugal</Descriptions.Item>
+        </Descriptions>
+        <Divider />
+    </Card>
   );
 }
 
