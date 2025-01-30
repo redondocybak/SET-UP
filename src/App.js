2f65ea48399a7f6af2816ca105170b40131f9dec
Alejandro Redondo Cybak <aredondocybak@gmail.com>
2025-01-30 15:11:14 -0500

    second part

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import { Card, Descriptions, Divider, List, Button} from 'antd';
-import dishes from '../src/assets/data/dishes.json'
-import DescriptionsItem from 'antd/es/descriptions/Item';
+import DetailedOrder from "./modules/DetailedOrder";
+
 
 function App() {
   return (
+   <DetailedOrder />
     
-    <Card title={'Order Title'} style={{ margin: 20 }}> 
-        <Descriptions bordered column={{lg: 1, md: 1, sm: 1}}>
-          <Descriptions.Item label='Customer'>Jose Ricardo Cuadra</Descriptions.Item>
-          <Descriptions.Item label='Customer Address'>Calle del Simio 4, Santo Domingo, Portugal</Descriptions.Item>
-        </Descriptions>
-        <Divider />
-    </Card>
+   
   );
 }
+
+
 
 export default App;
