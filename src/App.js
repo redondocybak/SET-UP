8abd32015276d2fd18d5e2aa45789a63d8aad1ae
Alejandro Redondo Cybak <aredondocybak@gmail.com>
2025-02-03 18:44:40 -0500

    february third

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import DetailedOrder from "./modules/DetailedOrder";
+import Orders from "./modules/Orders";
+import { Routes, Route } from 'react-router-dom';
+import{ Layout, Image } from 'antd';
 
+const { Sider, Content, Footer} = Layout;
 
 function App() {
   return (
-   <DetailedOrder />
-    
+    <Layout>
+      <Sider style={{ height: "100vh", backgroundColor: 'white'}}>
+        <Image src="c:\Users\aredo\Downloads\Uber-Eats-Symbol.jpg" preview={false}/>
+      </Sider>
+      <Layout>
+        <Content>
+        <Routes>
+      <Route path="" element={<Orders />} />
+      <Route path="order/:id" element={<DetailedOrder />}/>
+        </Routes>
+        </Content>
+        <Footer style={{ textAlign:'center'}}>
+          Uber Eats Restaurant Dashboard c2022
+        </Footer>
+      </Layout>
+    </Layout>
    
+    
   );
 }
 
 
 
 export default App;
+
