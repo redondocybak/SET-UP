930271ad703d3ab6b67661f765b72b588dba7f75
Alejandro Redondo Cybak <aredondocybak@gmail.com>
2025-02-05 16:03:08 -0500

    february fifth

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import DetailedOrder from "./modules/DetailedOrder";
 import Orders from "./modules/Orders";
+import RestaurantMenu from "./modules/RestaurantMenu";
 import { Routes, Route } from 'react-router-dom';
 import{ Layout, Image } from 'antd';
-
+import Sidemenu from "../src/components/Sidemenu";
 const { Sider, Content, Footer} = Layout;
 
 function App() {
   return (
     <Layout>
       <Sider style={{ height: "100vh", backgroundColor: 'white'}}>
-        <Image src="c:\Users\aredo\Downloads\Uber-Eats-Symbol.jpg" preview={false}/>
+        <Image src="https://logos-world.net/wp-content/uploads/2020/11/Uber-Eats-Symbol.jpg" preview={false}/>
+        <Sidemenu />
       </Sider>
       <Layout>
         <Content>
         <Routes>
       <Route path="" element={<Orders />} />
       <Route path="order/:id" element={<DetailedOrder />}/>
+      <Route path="menu" element={<RestaurantMenu />}/>
         </Routes>
         </Content>
         <Footer style={{ textAlign:'center'}}>
---
+++ src/components/Sidemenu/index.js
@@ -0,0 +1,32 @@
+import { Menu } from 'antd';
+import { useNavigate } from 'react-router-dom';
+
+const Sidemenu = () => {
+    const navigate = useNavigate();
+
+    const menuItems = [
+        {
+            key: "/",
+            label: "Orders"
+        },
+        {
+            key: "menu",
+            label: "Menu"
+        },
+        {
+            key: "order-history",
+            label: "Order History"
+        },
+        {
+            key: "settings",
+            label: "Settings"
+        }
+    ];
+
+    return (
+        <Menu items={menuItems} onClick={(menuItem) => navigate(menuItem.key)}/>
+    )
+    
+};
+
+export default Sidemenu;
